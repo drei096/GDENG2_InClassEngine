e1054c9a9c975f9421df95916b5c0ec6df771848
Aldrey Gaurana <gauranadrei@yahoo.com>
2022-10-17 15:51:43 +0800

    added depth buffer

--- DeviceContext.cpp
+++ DeviceContext.cpp
@@ -29,7 +29,9 @@ void DeviceContext::clearRenderTargetColor(SwapChain* swapChain, float r, float 
 {
 	FLOAT clear_color[] = { r,g,b,a };
 	m_device_context->ClearRenderTargetView(swapChain->renderTargetView, clear_color);
-	m_device_context->OMSetRenderTargets(1, &swapChain->renderTargetView, NULL);
+	m_device_context->ClearDepthStencilView(swapChain->depthStencilView, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1, 0);
+
+	m_device_context->OMSetRenderTargets(1, &swapChain->renderTargetView, swapChain->depthStencilView);
 	
 }
 
--- SwapChain.h
+++ SwapChain.h
@@ -12,8 +12,9 @@ public:
 	bool present(bool vsync);
 
 private:
-	IDXGISwapChain* swapChain;
-	ID3D11RenderTargetView* renderTargetView;
+	IDXGISwapChain* swapChain = nullptr;
+	ID3D11RenderTargetView* renderTargetView = nullptr;
+	ID3D11DepthStencilView* depthStencilView = nullptr;
 
 private:
 	friend class DeviceContext;
